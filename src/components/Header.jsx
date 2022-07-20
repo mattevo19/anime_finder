876f955ba330300de707d9b6a16f640e9b950563
matt <mattevo10@gmail.com>
2022-07-20 14:38:31 +0200

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="py-8">
       <h1 className="text-center text-6xl font-normal text-purple-900 underline decoration-pink-500/30">
-        Anime Hunter
+        Anime Hunter!
       </h1>
     </header>
   );
